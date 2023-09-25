3cfd9f701d7333c5ab950375dd0b4d26c3ff61d0
LalithyaMaddumage <sandeepanilalithya155@gmail.com>
2023-09-25 13:24:54 +0530

    backend folder structure and mongodb connection

---
+++ Backend/server.js
@@ -0,0 +1,38 @@
+// Importing required modules
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const app = express();
+
+// Loading environment variables
+dotenv.config();
+
+// Constants
+const PORT = process.env.PORT || 8070;
+const URL = process.env.MONGODB_URL;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+
+// Event listener for successful MongoDB connection
+connection.once("open", () => {
+  console.log("MongoDB connection successful!");
+});
+
+// use routes (if applicable)
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
