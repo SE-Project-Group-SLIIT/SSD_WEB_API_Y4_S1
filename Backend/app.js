8ce71d3d2989b31fde6dbb5176268df2cc10e20a
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-09-30 14:06:28 +0530

    add employee routes

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-var userRoutes = require('./routes/user/userMain');
+var userRoutes = require("./routes/user/userMain");
+var employeeRoutes = require("./routes/employee/employeeMain");
 
-router.use('/user', userRoutes);
 
+router.use("/user", userRoutes);
+router.use("/employee", employeeRoutes);
 
 module.exports = router;
